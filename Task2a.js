616527a7dd445285c6ad85409d1cc15de994284d
simon-lock <simonlock6@hotmail.com>
2015-11-16 19:38:25 +0000

    Completed Task2b and split out func in new file.

--- Task2a.js
+++ Task2a.js
@@ -1,85 +1,8 @@
 var fs = require('fs');
+var calc = require("./BestAndWorst.js");
 
 // Couldn't get async version to run :( Kept saying data was undefined.
 // Using sync instead.
-var jsonData = JSON.parse(fs.readFileSync('AppleData.json', 'utf8')).dataset;
+var fileData = fs.readFileSync('AppleData.json', 'utf8');
 
-//console.log(jsonData);
-
-var dateIndex = jsonData.column_names.indexOf('Date');
-var adjCloseIndex = jsonData.column_names.indexOf('Adj. Close');
-var daySpan = 7;
-var dataSize = jsonData.data.length;
-
-var bestPerformance = Number.MIN_VALUE;
-var bestPerformanceDate = Date.MIN_VALUE;
-var worstPerformance = Number.MAX_VALUE;
-var worstPerformanceDate = Date.MIN_VALUE;
-
-jsonData.data.forEach(function(currentDayData, index, source) {
-
-  var currentDate = new Date(currentDayData[dateIndex]);
-  var targetDate = new Date(currentDate);
-  targetDate.setDate(targetDate.getDate() - daySpan);
-
-  // Find target date's data.
-
-  var searching = true;
-  var found = false;
-
-  var walkedIndex = index + 1;
-  var maxWalkedIndex = walkedIndex + daySpan;
-
-  var previousDayData;
-
-  while (searching) {
-    if (walkedIndex >= dataSize || walkedIndex > maxWalkedIndex) {
-      // Walked too far.
-      searching = false;
-    } else {
-      var walkedData = source[walkedIndex];
-      var walkedDate = new Date(walkedData[dateIndex]);
-
-      if (walkedDate >= targetDate) {
-        previousDayData = walkedData;
-        found = true;
-      } else {
-        searching = false;
-      }
-    }
-    walkedIndex++;
-  }
-
-  // Calculate stats.
-
-  if (found) {
-    var currentClose = currentDayData[adjCloseIndex];
-    var previousClose = previousDayData[adjCloseIndex];
-
-    var delta = currentClose - previousClose;
-    var deltaPct = (delta / previousClose) * 100;
-
-    if (deltaPct > bestPerformance) {
-      bestPerformance = deltaPct;
-      bestPerformanceDate = currentDate;
-    }
-    if (deltaPct < worstPerformance) {
-      worstPerformance = deltaPct;
-      worstPerformanceDate = currentDate;
-    }
-
-    console.log();
-    console.log("Current Date: " + currentDate + ", Close: " + currentClose);
-    console.log("Previous Date: " + previousDayData[dateIndex] + ", Close: " + previousClose);
-    console.log("Delta %: " + deltaPct);
-  } else {
-    console.log("Could not find any data for start date " + currentDate);
-  }
-});
-
-console.log();
-console.log();
-console.log("Best Performance Date: " + bestPerformanceDate);
-console.log("Delta: " + bestPerformance);
-console.log("Worst Performance Date: " + worstPerformanceDate);
-console.log("Delta: " + worstPerformance);
+calc.parseJsonAndCalcBestAndWorst(fileData, 7)
--- Task2b.js
+++ Task2b.js
@@ -1,4 +1,5 @@
-var http = require("http");
+var https = require("https");
+var calc = require("./BestAndWorst.js");
 
 if (process.argv.length < 4) {
   console.log("Invalid arguments. Process requires dataset and api key.");
@@ -10,11 +11,20 @@ var apiKey = process.argv[3];
 
 var options = {
   host: "www.quandl.com",
-  path: "/api/v3/datasets/WIKI/" + dataSet + "?api_key=" + apiKey
+  path: "/api/v3/datasets/WIKI/" + dataSet + "?api_key=" + apiKey,
+  method: 'GET'
 };
 
-callback = function(response) {
-  console.log(response);
-}
+var getReq = https.get(options, function(response) {
+  // Continuously update stream with data
+  var body = '';
+  response.on('data', function(d) {
+    body += d;
+  });
+  response.on('end', function() {
 
-http.request(options, callback).end();
+    //console.log(body);
+    calc.parseJsonAndCalcBestAndWorst(body, 7)
+
+  });
+});
