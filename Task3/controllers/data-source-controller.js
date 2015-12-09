d3d7fdd385434fb1e07d93125366d1aa26b898d5
simon-lock <simonlock6@hotmail.com>
2015-12-09 23:15:18 +0000

    got it working

--- Task3/controllers/data-source-controller.js
+++ Task3/controllers/data-source-controller.js
@@ -3,25 +3,25 @@ angular.module('dataSourceControllers', [])
   .controller('DataSourceCtrl', ['$scope', function($scope) {
     var that = this;
 
-    var dataSets =
+    that.dataSets =
     [
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
-      {title:"Hello, World!" body:"WAZZUP!"},
-      {title:"Hello again, World!" body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"},
+      {title:"Hello, World!", body:"WAZZUP!"},
+      {title:"Hello again, World!", body:"WAZZUP NOW!"}
     ];
   }]);
